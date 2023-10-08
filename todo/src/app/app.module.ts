0b6ce79c4eb9545e308eece973b74ca82a5fa7fb
harikrishnan <appuzz2712@gmail.com>
2023-10-08 23:38:38 +0530

    workitem components creation

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layouts/navbar/navbar.component';
+import { FotterComponent } from './components/layouts/fotter/fotter.component';
+import { CategoryComponent } from './components/category/category.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FotterComponent,
+    CategoryComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
